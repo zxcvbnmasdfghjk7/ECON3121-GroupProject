0e165a68cbd873a0bf2ae7cbf5badf134e265908
zxcvbnmasdfghjk7 <30559794+zxcvbnmasdfghjk7@users.noreply.github.com>
2026-04-21 22:47:58 +1000

    Resolve: Conflicting Commit History between Local Repository and Github Repository

---
+++ src/OLS_Reg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 23:49:53 2026
+
+@author: eddie
+"""
+
+import statsmodels.api as sm
+import pandas
+from patsy import dmatrices
+
+aapl
--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,34 @@ Created on Tue Apr 21 06:55:59 2026
 @author: edmundzhou
 """
 
+<<<<<<< HEAD:src/__init__.py
+=======
+import pandas as pd
+import numpy as np
+
+# Load Market Share Dataset.
+aapl_market_share = pd.read_csv("datasets/raw/vendor-ww-quarterly-20131-20262.csv")
+
+# Select only Date and Apple
+aapl_market_share = (aapl_market_share.filter(items = ['Date', 'Apple'])
+                     # Rename Date to Quarter
+                     .rename(columns = {'Date': 'Quarter'})
+                     # Rename Apple to Market Share
+                     .rename(columns = {'Apple': 'Market_Share'}))
+
+# Reformat Quarter
+aapl_market_share['Quarter'] = pd.PeriodIndex(aapl_market_share['Quarter'], freq = "Q").strftime('Q%q %Y')
+
+# Load Sales Revenue Dataset
+aapl_sales = (pd.read_excel(sheet_name = "Data", 
+                           io = "datasets/raw/statistic_id382136_apple-net-sales-quarterly-fy-2012-2026-by-operating-segment.xlsx", 
+                           usecols = "B:G", skiprows = 4)
+              .rename(columns = {'Unnamed: 1': 'Quarter'})
+              .filter(items = ['Quarter', 'iPad*'])
+              .rename(columns = {'iPad*': 'Sales_Revenue'})
+              .iloc[4:])
+
+aapl = pd.merge(aapl_market_share, aapl_sales, 
+                on = "Quarter", 
+                how = "inner")
+>>>>>>> 9b75acd (Update Repository: Fix Commit History):process_datasets.py
