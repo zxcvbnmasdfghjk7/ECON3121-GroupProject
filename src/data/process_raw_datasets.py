b4be7bb426efbb2dd289f018b9b75c4f16ad77a3
zxcvbnmasdfghjk7 <30559794+zxcvbnmasdfghjk7@users.noreply.github.com>
2026-04-25 16:38:16 +1000

    Resolve Sync Issue

--- src/data/process_raw_datasets.py
+++ src/data/process_raw_datasets.py
@@ -5,17 +5,17 @@ Created on Tue Apr 21 11:46:54 2026
 
 @author: edmundzhou
 """
-
 # Import Packages
 from src import config
 import pandas as pd
 
 # Import AAPL's Market
 market_share = pd.read_csv(config.DATA_DIRECTORY / "raw" / "vendor-ww-quarterly-20131-20262.csv")
-market_share = (market_share[['Date', 'Apple']]
-                .rename(columns = {'Date': 'Quarter'}))
+market_share = (market_share.filter(items = ['Date', 'Apple'])
+                .rename(columns = {'Date': 'Quarter'})
+                .rename(columns = {'Apple': 'Market_Share'}))
 
-market_share['Quarter'] = market_share['Quarter'].str.replace('-', ' Q', regex = False)
+market_share['Quarter'] = pd.PeriodIndex(market_share['Quarter'], freq = "Q").strftime('Q%q %Y')
 
 # Import Statista's Sales Revenue for IPad AAPL
 sales_revenue = (pd.read_excel(config.DATA_DIRECTORY / "raw" / "statistic_id382136_apple-net-sales-quarterly-fy-2012-2026-by-operating-segment.xlsx", 
@@ -27,4 +27,4 @@ sales_revenue = (pd.read_excel(config.DATA_DIRECTORY / "raw" / "statistic_id3821
 
 sales_revenue = sales_revenue[['Quarter', 'Sales_Revenue']]
 
-
+aapl = pd.merge(sales_revenue, market_share, on = 'Quarter', how = 'inner')
